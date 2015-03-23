26df68f7659622742673d7385cdc86dc4fc3370e
Jack Pearkes <jackpearkes@gmail.com>
2015-03-22 20:25:53 -0400

    ui: improve views for sealed / unsealed state

--- ui/app/views/vault.js
+++ ui/app/views/vault.js
@@ -2,10 +2,25 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
   templateName: function() {
-    if (this.get('content.unsealed')) {
+    // Show the right view based on the seal state. If the seal
+    // is undefined or otherwise unknown, raise an error.
+    //
+    if (this.get('controller.model.unsealed') === true) {
       return "vault-unsealed";
-    } else {
+    } else if (this.get('controller.model.unsealed') === false) {
       return "vault-sealed";
+    } else {
+      throw new Error('Incompatible seal response when looking up view');
     }
-  }.property('content.unsealed')
+  }.property('controller.model.unsealed'),
+
+  // Rerenders the view then the seal status changes.
+  // We call rerender() here because we're actually switching
+  // templates.
+  //
+  watchSealStatus: function() {
+    this.rerender();
+
+    // TODO animate change of templates
+  }.observes('controller.model.unsealed'),
 });
